30b95fd74e2659cbe7ba92e3d454ae5012141385
Derrick Foster <derrick.j.foster@gmail.com>
2019-08-24 13:02:03 -0600

    added project thumbnails

--- portfolio_site/src/components/projects.js
+++ portfolio_site/src/components/projects.js
@@ -1,16 +1,28 @@
 import React, { Component } from 'react'
 import '../App.css'
+import Ironximpact from '../Images/ironximpact.png'
+import KickstART from '../Images/kickstART.png'
 
 class Projects extends Component {
   render() {
     return (
-      <>
-        <div classname="projects">
-          <h1>Featured Projects</h1>
+      <div className='projects'>
+        <h1>Featured Projects</h1>
+        <div className="container">
 
+            <ul>
+              <li>
+                <a href='https://derrickjfoster.github.io/ironximpact/#/'><img className='thumbnail'src={Ironximpact}></img></a>
+                {/* <img className='thumbnail'src={Ironximpact}></img> */}
+              </li>
+
+              <li>
+                <a href='https://derrickjfoster.github.io/ironximpact/#/'><img className='thumbnail'src={KickstART}></img></a>
+              </li>
+            </ul>
 
         </div>
-      </>
+      </div>
     )
   }
 }
