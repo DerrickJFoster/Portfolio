5b7802a715c5c75e0d6bc65b79607a9f9d58287d
Derrick Foster <derrick.j.foster@gmail.com>
2019-08-31 11:30:04 -0600

    Updates

---
+++ precache-manifest.18146596638508cd897edcd8eb0fa7fb.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fe7a68eee3d0ef1fbe1b47c89991477",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "93e767fe28fd45330f95",
+    "url": "/Portfolio/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "5eb392afa62b05a12ed3",
+    "url": "/Portfolio/static/css/main.c315a5ce.chunk.css"
+  },
+  {
+    "revision": "93e767fe28fd45330f95",
+    "url": "/Portfolio/static/js/2.31c7a0bb.chunk.js"
+  },
+  {
+    "revision": "5eb392afa62b05a12ed3",
+    "url": "/Portfolio/static/js/main.aa5a3cb8.chunk.js"
+  },
+  {
+    "revision": "faadd0d3320662daaa57",
+    "url": "/Portfolio/static/js/runtime~main.21d9ec9f.js"
+  },
+  {
+    "revision": "a757e81ba11c45430ae546cdae98fd96",
+    "url": "/Portfolio/static/media/dreamcatcher.a757e81b.png"
+  },
+  {
+    "revision": "a6cc108cbd81025d47eecaa88a8eb11b",
+    "url": "/Portfolio/static/media/ironximpact.a6cc108c.png"
+  },
+  {
+    "revision": "60359064e4307dd86d372602f8fd1a88",
+    "url": "/Portfolio/static/media/kickstART.60359064.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
