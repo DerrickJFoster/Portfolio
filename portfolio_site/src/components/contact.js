1e5f09d24760f499fa77843d75028c4da61403a3
Derrick Foster <derrick.j.foster@gmail.com>
2019-09-09 17:59:24 -0600

    fixed project links

--- portfolio_site/src/components/contact.js
+++ portfolio_site/src/components/contact.js
@@ -5,11 +5,11 @@ class Contact extends Component {
     return (
       <div className='contact'>
       <h1>Contact</h1>
-      <br/>
       {/* <h3>Get in touch</h3> */}
         <h6>970•631•7889</h6>
         <h6>Derrick.J.Foster@gmail.com</h6>
         <h6><a href='https://www.linkedin.com/in/derrickjonfoster/'>LinkedIn</a></h6>
+        <br/>
       </div>
     )
   }
--- portfolio_site/src/components/projects.js
+++ portfolio_site/src/components/projects.js
@@ -2,28 +2,27 @@ import React, { Component } from 'react'
 import '../App.css'
 import Ironximpact from '../Images/ironximpact.png'
 import KickstART from '../Images/kickstART.png'
-
+import Dreamcatcher from '../Images/dreamcatcher.png'
 class Projects extends Component {
   render() {
     return (
-      <div className='projects'>
+      <>
+      <div className='projects-header'>
         <h1>Featured Projects</h1>
+
         <hr />
         <div className="container">
-            <ul>
-              <li>
-                <a href='https://derrickjfoster.github.io/ironximpact/#/'><img className='thumbnail'src={Ironximpact}></img></a>
-                {/* <img className='thumbnail'src={Ironximpact}></img> */}
-              </li>
+          <a href='https://derrickjfoster.github.io/ironximpact/#/'><img className='thumbnail'src={Ironximpact}></img></a>
 
-              <li>
-                <a href='https://derrickjfoster.github.io/ironximpact/#/'><img className='thumbnail'src={KickstART}></img></a>
-              </li>
-            </ul>
+          <a href='https://derrickjfoster.github.io/Artsy-Front-End/'><img className='thumbnail'src={KickstART}></img></a>
 
+          <a href='https://mydreamcatcher.herokuapp.com/'><img className='thumbnail'src={Dreamcatcher}></img></a>
+
+          <hr/>
         </div>
-        <hr/>
       </div>
+      <hr/>
+      </>
     )
   }
 }
